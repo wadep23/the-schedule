11cf3abc4171616bb535c453b498d6358876faec
wadep <wadepfacer@gmail.com>
2021-04-18 14:01:06 -0600

    added date heading to page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var dateEl = document.querySelector('#date');
+var timeEl = document.querySelector('#time-stamp');
+
+var currentDate = new Date();
+console.log(currentDate);
+
+dateEl.innerHTML = 'Today: ' + currentDate.toDateString();
+
+var timeLoop = funtion() {
+    
+};
