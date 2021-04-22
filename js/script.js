c7e19fcc1c3861da7001a0fbcf6490130e1577d0
wadep <wadepfacer@gmail.com>
2021-04-21 20:38:55 -0600

    finial tweaks

--- js/script.js
+++ js/script.js
@@ -1,8 +1,45 @@
-var dateEl = document.querySelector('#date');
-var timeEl = document.querySelector('#time-stamp');
+var currentDate = document.querySelector(".current-date");
+currentDate.textContent = moment().format("dddd, MMMM Do");
 
-var currentDate = new Date();
-console.log(currentDate);
 
-dateEl.innerHTML = 'Today: ' + currentDate.toDateString();
+$(".saveBtn").on("click", function(){
+    var task = $(this)
+        .siblings(".details")
+        .val();
 
+    console.log(task);
+    //create another for parent element to grab id. pass function to save to local storage on click
+    var save = $(this)
+        .siblings(".details")
+        .attr("id")
+    console.log(save);
+
+    localStorage.setItem(save, task);
+    
+});
+
+//if past: grey. if present: red. if future: green.
+function timeAlert(){
+    //moment function to create time association for each hour
+    
+    for (i = 0; i < 25; i++){
+       
+        if (moment().format("H") > i){
+            $(`#${i}`).addClass("past");
+        } else if (moment().format("H") < i){
+            $(`#${i}`).addClass("future");
+        } else{
+            $(`#${i}`).addClass("current");
+        };
+         
+        $(`#${i}`)
+            .val(localStorage.getItem(i));
+    }      
+};
+//if button clicked, add to local storage. Retrieve from local storage and 
+//display in specified time
+
+
+
+
+timeAlert();
